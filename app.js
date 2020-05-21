e34e9ec5ccc95d2c73b45aee42d76d4ec782ebdd
David Thatcher <54860843+Davidathatch@users.noreply.github.com>
2020-05-21 11:47:30 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 $(".info-card").addClass("info-card-fade");
+
+$(".droptdown-button").on("click", function () {
+  if ($(".services ul").css("opacity") == 0) {
+    $(".services ul").css("opacity", "1");
+    $(".services ul").css("pointerEvents", "all");
+    $(".services ul").css("transform", "translateY(-10px)");
+  } else {
+    $(".services ul").css("opacity", "0");
+    $(".services ul").css("pointerEvents", "none");
+    $(".services ul").css("transform", "translateY(10px)");
+  }
+});
+
+$("html").on("click", function () {
+  if ($(".services ul").css("opacity") == 1) {
+    $(".services ul").css("opacity", "0");
+    $(".services ul").css("pointerEvents", "none");
+    $(".services ul").css("transform", "translateY(10px)");
+  }
+});
